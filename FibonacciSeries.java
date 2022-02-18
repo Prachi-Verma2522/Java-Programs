a59d177cda7d1ed7908c2fc288abc17b01b6e6ce
Prachi Verma <56695669+Prachi-Verma2522@users.noreply.github.com>
2022-02-19 02:00:53 +0530

    Create FibonacciSeries.java

---
+++ FibonacciSeries.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+	static int fib(int n) {
+	/* Declare an array to store Fibonacci numbers. */
+	int f[] = new int[n+2]; // 1 extra to handle case n = 0
+	int i;
+	
+	/* 0th and 1st number of the series are 0 and 1*/
+	f[0] = 0;
+	f[1] = 1;
+	
+	for (i = 2; i <= n; i++)
+	{
+	/* Add the previous 2 numbers in the series
+		and store it */
+		f[i] = f[i-1] + f[i-2];
+	}
+	
+	return f[n];
+	}
+	
+	public static void main (String args[])
+	{
+		//int n = 10;
+		Scanner sc= new Scanner(System.in);
+		int n= sc.nextInt();
+		System.out.println(fib(n));
+		// by dp
+		// T.C- O(N);
+	}
+}
+
+
